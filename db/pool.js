b19752f5bd9787f5b1c083cd3e93e7cffbd8127f
Muhammed Said Gülberk <62105391+MSaidG@users.noreply.github.com>
2024-12-19 16:57:54 +0300

    Change deploy script and add remote database

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,15 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   user: process.env.DB_USER || 'postgres',
+//   host: process.env.DB_HOST || 'localhost',
+//   database: process.env.DB_NAME || 'postgres',
+//   password: process.env.DB_PASSWORD || 'postgres',
+//   port: process.env.DB_PORT || 5432,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  port: process.env.DB_PORT || 5432,
-});
+  connectionString: process.env.MOVIE_DB_URI,
+})
 
 module.exports = pool;
